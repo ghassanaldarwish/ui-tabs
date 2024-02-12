cc8edfecd5adb2512aa67af7d173ba710f25c886
Ghassanooooo <ghassanaldarwish@yahoo.com>
2024-02-12 17:11:48 +0100

    first commit

---
+++ dist/types.d.ts
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as TabsPrimitive from '@radix-ui/react-tabs';
+
+declare const Tabs: React.ForwardRefExoticComponent<TabsPrimitive.TabsProps & React.RefAttributes<HTMLDivElement>>;
+declare const TabsList: React.ForwardRefExoticComponent<Omit<any, "ref"> & React.RefAttributes<unknown>>;
+declare const TabsTrigger: React.ForwardRefExoticComponent<Omit<any, "ref"> & React.RefAttributes<unknown>>;
+declare const TabsContent: React.ForwardRefExoticComponent<Omit<any, "ref"> & React.RefAttributes<unknown>>;
+
+export { Tabs, TabsContent, TabsList, TabsTrigger };
